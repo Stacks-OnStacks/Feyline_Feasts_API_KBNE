30f9e4df52bb6f52b86102216c0dfa615838c25f
NickolasEarles <nickolasearles@gmail.com>
2022-08-25 10:30:44 -0400

    postmerge3

--- FF_API_KBNE/src/main/java/com/revature/project_1/Dish/DishServlet.java
+++ FF_API_KBNE/src/main/java/com/revature/project_1/Dish/DishServlet.java
@@ -1,16 +1,14 @@
-package com.revature.project_1.Orders;
+package com.revature.project_1.Dish;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.project_1.Dish.DTO.requests.EditDishRequest;
 import com.revature.project_1.Dish.DTO.requests.NewDishIDRequest;
 import com.revature.project_1.Dish.DTO.response.DishResponse;
-import com.revature.project_1.Dish.DishService;
 import com.revature.project_1.util.exceptions.InvalidUserInputException;
 import com.revature.project_1.util.exceptions.ResourcePersistanceException;
 import com.revature.project_1.util.interfaces.Authable;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,14 +17,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-public class OrderServlet extends HttpServlet  implements Authable {
+public class DishServlet extends HttpServlet  implements Authable {
 
-    private final OrderService orderService;
+    private final DishService dishService;
     private final ObjectMapper objectMapper;
     private final Logger logger= LogManager.getLogger();
 
-    public OrderServlet(OrderService orderService, ObjectMapper objectMapper) {
-        this.orderService = orderService;
+    public DishServlet(DishService dishService, ObjectMapper objectMapper) {
+        this.dishService = dishService;
         this.objectMapper = objectMapper;
 
     }
@@ -34,25 +32,25 @@ public class OrderServlet extends HttpServlet  implements Authable {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String id = req.getParameter("orderId");
+        String id = req.getParameter("dishId");
 
         if(id!=null) {
             try {
-                OrderResponse order = orderService.findByOrderID(id);
+                DishResponse dish = dishService.findByDishID(id);
 
-                String payload = objectMapper.writeValueAsString(order);
+                String payload = objectMapper.writeValueAsString(dish);
                 resp.getWriter().write(payload);
 
             } catch (InvalidUserInputException e) {
-                logger.warn("order information entered was not reflective of any dish in the database. id provided was: {}", id);
-                resp.getWriter().write("order entered was not in the database.");
+                logger.warn("dish information entered was not reflective of any dish in the database. id provided was: {}", id);
+                resp.getWriter().write("dish entered was not in the database.");
                 resp.setStatus(404);
             }
         }
         else {
-            List<OrderResponse> order = orderService.readAll();
+            List<DishResponse> dish = dishService.readAll();
 
-            String payload = objectMapper.writeValueAsString(order);
+            String payload = objectMapper.writeValueAsString(dish);
 
             resp.getWriter().write(payload);
         }
@@ -63,14 +61,14 @@ public class OrderServlet extends HttpServlet  implements Authable {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         PrintWriter respWriter =resp.getWriter();
-        NewOrderIDRequest order= objectMapper.readValue(req.getInputStream(), NewOrderIDRequest.class);
+        NewDishIDRequest dish= objectMapper.readValue(req.getInputStream(), NewDishIDRequest.class);
 
 
         try{
-            logger.info("order request to add the following to the database: {}", order);
-            DishResponse newDish = orderService.registerDish(dish);
+            logger.info("user request to add the following to the database: {}", dish);
+            DishResponse newDish = dishService.registerDish(dish);
 
-            String payload= objectMapper.writeValueAsString(newOrder);
+            String payload= objectMapper.writeValueAsString(newDish);
 
             respWriter.write(payload);
             resp.setStatus(201);
@@ -88,11 +86,11 @@ public class OrderServlet extends HttpServlet  implements Authable {
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        EditOrderRequest editOrder = objectMapper.readValue(req.getInputStream(), EditDishRequest.class);
+        EditDishRequest editDish = objectMapper.readValue(req.getInputStream(), EditDishRequest.class);
 
         try {
-            orderService.update(editOrder);
-            resp.getWriter().write("Order update successful");
+            dishService.update(editDish);
+            resp.getWriter().write("User update successful");
         } catch (InvalidUserInputException e) {
             resp.getWriter().write(e.getMessage());
             resp.setStatus(404);
@@ -106,14 +104,17 @@ public class OrderServlet extends HttpServlet  implements Authable {
     protected void doDelete (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
         if (!checkAuth(req, resp)) return;
-        String orderId = req.getParameter("OrderId");
-        if (orderId != null) {
-            orderService.remove(orderId);
-            resp.getWriter().write("User with " + orderId + " has been deleted");
+        String dishId = req.getParameter("username");
+        if (dishId != null) {
+            dishService.remove(dishId);
+            resp.getWriter().write("User with " + dishId + " has been deleted");
         } else {
             resp.getWriter().write("This request requires an email parameter in the path ?email=example@mail.com");
             resp.setStatus(400);
         }
     }
-
 }
+
+
+
+
